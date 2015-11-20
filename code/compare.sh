60da163ed3f4672329cd0e96c4467d2725ebac2e
NonWhite <wperezurcia@gmail.com>
2015-11-20 00:43:03 -0200

    compare astar results for each dataset

--- code/compare.sh
+++ code/compare.sh
@@ -1,6 +1,8 @@
-sets=( census letter image mushroom sensors steelPlates epigenetics alarm spectf lungCancer )
+sets=( census voting letter hepatitis image heart mushroom parkinsons autos flag )
+RESULTS_DIR="../results/"
 
 for i in "${sets[@]}"
 do
-	wc "../data/${i}_scores.txt" "$IME_HOME/Pesquisas/BN_Initializer/data/${i}_scores.txt"
+	echo " ============================== ${i} ============================== "
+	head -n5 "${RESULTS_DIR}${i}_simple.txt" "${RESULTS_DIR}${i}_dynamic_k2.txt" "${RESULTS_DIR}${i}_dynamic_k3.txt" "${RESULTS_DIR}${i}_dynamic_k4.txt" "${RESULTS_DIR}${i}_static_d2.txt" "${RESULTS_DIR}${i}_static_d3.txt"
 done
